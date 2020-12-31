fdaa8212753c2a061e4968576b271bc0b364db37
Vinicius Pereira <agavi2014@hotmail.com>
2020-12-30 22:07:56 -0300

    [NEW] List, Add and Remove

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from "react";
+import React ,{useState, useEffect}from "react";
 
 import "./styles.css";
+import api from "./services/api"
 
 function App() {
+
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+    api.get("/repositories")
+    .then(response => {
+      setRepositories(response.data)
+    })
+  })
+
   async function handleAddRepository() {
-    // TODO
+    await api.post("/repositories", {
+      url: Date.now(),
+      title: "Repo "+Date.now(),
+      techs: ["eetet", "qeqtte"]
+    })
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete("/repositories/"+id)
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map( repository => (
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+          <li key={repository.id}>
+            {repository.title}
+
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
